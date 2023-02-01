f6bd6988eafc9873cec9e77c45d5e749e6a2b2b6
mister <souleymane06.ba@gmail.com>
2023-02-01 12:48:36 +0100

    fonction ajouté

--- c++/life_cycle.cpp
+++ c++/life_cycle.cpp
@@ -10,12 +10,36 @@ using namespace std;
 
 class Animal{
     private:
-        const string name;
-        int pv;
+        string name;
+        int pv,x,y;
         string sx;
+    public:
+        Animal(const string &n,int p, const string &s,int px, int py){
+            name = n;
+            pv = p;
+            sx = s;
+            x = px;
+            y = py;
+        }
+
+        string get_name(){
+            return name;
+        }
+
+        int* get_pos(){
+            static int pos[2] = {y,x};
+            return pos;
+        }
+
+        bool dead(){
+            return pv<=0;
+        }
 };
 
 int main(){
-
+    Animal a("M",5,"F",1,4);
+    int *pos = a.get_pos();
+    cout<<pos[0]<<pos[1];
+    cout<<a.dead();
     return 0;
 }
