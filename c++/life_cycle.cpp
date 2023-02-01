fd009e0247d3587a2c37030186745f8ea541c696
mister <souleymane06.ba@gmail.com>
2023-02-01 12:15:21 +0100

    début

---
+++ c++/life_cycle.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<cstdio>
+#include<string>
+#include <stdlib.h>
+#include <vector>
+#include <math.h> 
+
+
+using namespace std;
+
+class Animal{
+    private:
+        const string name;
+        int pv;
+        string sx;
+};
+
+int main(){
+
+    return 0;
+}
